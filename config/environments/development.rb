4b6df09b26f6be5346d232d3b802c68313bdc24e
Ubuntu <ubuntu@ip-172-31-5-174.us-west-1.compute.internal>
2023-10-09 03:26:48 +0000

    create programs resource and add action text

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,7 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts << "315adaf307bd47969002b67b5a5df3ed.vfs.cloud9.us-west-1.amazonaws.com"
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
